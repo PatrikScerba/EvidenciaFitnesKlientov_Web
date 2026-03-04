daacbe9be10b7a22a63e2fea35a9a9ca2acb5101
Patrik Scerba <patrikscerba@gmail.com>
2026-03-04 18:44:02 +0100

    chore: inicializácia Spring Boot projektu

---
+++ src/test/java/sk/patrikscerba/gym/GymManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.patrikscerba.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
