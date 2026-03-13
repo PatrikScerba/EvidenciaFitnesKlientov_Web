4ea1bfd17e4d65bddff30958c472a4e7d6c75a14
Patrik Scerba <patrikscerba@gmail.com>
2026-03-13 14:29:35 +0100

    feat(frontend): inicializacia React frontendu a priprava zakladnej struktury

---
+++ react-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Gym Management System</h1>
+      <h2>Klienti</h2>
+      <p>React frontend beží</p>
+      </div>
+  );
+}
+
+export default App;
