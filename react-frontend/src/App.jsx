f4037ec51f2d28d92c60dbbd8d219f30fea9df17
Patrik Scerba <patrikscerba@gmail.com>
2026-03-17 17:08:16 +0100

    feat(react-auth): pridane odhlasenie pouzivatela

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
+import { logout } from "./api/authApi";
 import Login from "./pages/auth/Login";
 
 export default function App() {
     const [user, setUser] = useState(null);
 
+    async function handleLogout() {
+        try {
+            await logout();
+        } catch (err) {
+            console.error("Odhlásenie zlyhalo:", err);
+        } finally {
+            setUser(null);
+        }
+    }
+
     return (
+
         <div style={{ padding: "30px", fontFamily: "Arial" }}>
             {user ? (
                 <div>
                     <h1>React ↔ Spring Boot</h1>
                     <p>Používateľ je prihlásený.</p>
                     <pre>{JSON.stringify(user, null, 2)}</pre>
+
+                    <button onClick={handleLogout}>
+                        Odhlásiť sa
+                    </button>
                 </div>
             ) : (
                 <Login onLoginSuccess={setUser} />
