5b9045440fa9c6b7b9dff2c29b49497c30522059
Patrik Scerba <patrikscerba@gmail.com>
2026-09-09 07:19:27 +0200

    feat(email): pridanie odosielania emailov vybraným klientom

--- src/main/java/sk/patrikscerba/gym/service/email/NotificationEmailServiceImpl.java
+++ src/main/java/sk/patrikscerba/gym/service/email/NotificationEmailServiceImpl.java
@@ -1,7 +1,13 @@
 package sk.patrikscerba.gym.service.email;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import sk.patrikscerba.gym.dto.email.EmailRequest;
+import sk.patrikscerba.gym.dto.email.EmailSendRequest;
+import sk.patrikscerba.gym.entity.ClientEntity;
 import sk.patrikscerba.gym.repository.ClientRepository;
+
+import java.util.List;
 
 @Service
 public class NotificationEmailServiceImpl implements NotificationEmailService {
@@ -13,5 +19,23 @@ public class NotificationEmailServiceImpl implements NotificationEmailService {
                                         ClientRepository clientRepository) {
         this.emailService = emailService;
         this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public void sendEmail(EmailSendRequest request, List<MultipartFile> attachments) {
+
+        List<ClientEntity> clients =
+                clientRepository.findAllById(request.getClientIds());
+
+        for (ClientEntity client : clients) {
+            EmailRequest emailRequest = new EmailRequest();
+
+            emailRequest.setTo(client.getEmail());
+            emailRequest.setRecipientName(client.getFirstName());
+            emailRequest.setSubject(request.getSubject());
+            emailRequest.setMessage(request.getMessage());
+
+            emailService.sendEmail(emailRequest, attachments);
+        }
     }
 }
